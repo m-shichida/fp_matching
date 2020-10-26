bfbdc35bdec81dfe0bd3c840b1c739bac3f60e24
ShichidaMasatsugu <shicchi.7913@icloud.com>
2020-10-27 08:19:47 +0900

    #31 migrate実行時にannotateを実行するようにした

--- spec/models/financial_planner_specialty_spec.rb
+++ spec/models/financial_planner_specialty_spec.rb
@@ -3,10 +3,18 @@
 # Table name: financial_planner_specialties
 #
 #  id                        :bigint           not null, primary key
-#  financial_planner_post_id :bigint           not null              # ファイナンシャルプランナー投稿ID
 #  name                      :string(255)      not null              # 得意分野名
 #  created_at                :datetime         not null
 #  updated_at                :datetime         not null
+#  financial_planner_post_id :bigint           not null              # ファイナンシャルプランナー投稿ID
+#
+# Indexes
+#
+#  index_financial_planner_specialties_on_financial_planner_post_id  (financial_planner_post_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (financial_planner_post_id => financial_planner_posts.id)
 #
 require 'rails_helper'
 
