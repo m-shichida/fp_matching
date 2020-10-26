81ab63a18c777817b788d4238d84739784276afd
ShichidaMasatsugu <shicchi.7913@icloud.com>
2020-10-26 09:00:10 +0900

    #12 検索フォーム作成

--- app/controllers/financial_planner_posts_controller.rb
+++ app/controllers/financial_planner_posts_controller.rb
@@ -2,11 +2,19 @@ class FinancialPlannerPostsController < ApplicationController
   before_action :authenticated!, only: :index
 
   def index
-    @q = FinancialPlannerPost.ransack(params[:q])
+    @q = FinancialPlannerPost.ransack(search_params)
     @posts = @q.result(distinct: true)
   end
 
   def new
     @post = FinancialPlannerPost.new
+  end
+
+  private
+
+  def search_params
+    return {} if params[:q].blank?
+
+    params.require(:q).permit(:place_cont, :specialties_name_cont, :financial_planner_gender_eq).to_h
   end
 end
