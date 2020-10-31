0109b35b879d968676c2dec5ebec51c0c06f82e5
ShichidaMasatsugu <shicchi.7913@icloud.com>
2020-10-31 18:59:14 +0900

    customerとfpのログイン状態によってエラーが発生していたので修正

--- app/helpers/financial_planner_posts_helper.rb
+++ app/helpers/financial_planner_posts_helper.rb
@@ -1,5 +1,5 @@
 module FinancialPlannerPostsHelper
-  def current_customer_not_appoint?(post)
-    current_customer && post.financial_planner.appointments.appointed_by_customer(current_customer).empty?
+  def not_appoint?(post)
+    post.financial_planner.appointments.appointed_by_customer(current_customer).empty?
   end
 end
